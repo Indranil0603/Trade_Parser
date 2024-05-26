a9a3539c87c44d4ac294b7faa62bdf07c7e4bfba
Indranil0603 <indranilsen1983@gmail.com>
2024-05-26 09:20:00 +0530

    Add utility for parsing csv files

---
+++ utils/csvParser.js
@@ -0,0 +1,25 @@
+const csv = require("csv-parser");
+const fs = require("fs");
+
+const parseCSV = (filePath) => {
+	return new Promise((resolve, reject) => {
+		const results = [];
+		fs.createReadStream(filePath)
+			.pipe(csv())
+			.on("data", (data) => {
+				const [base_coin, quote_coin] = data.Market.split("/");
+				results.push({
+					utc_time: new Date(data.UTC_Time),
+					operation: data.Operation,
+					base_coin: base_coin.trim(),
+					quote_coin: quote_coin.trim(),
+					amount: parseFloat(data["Buy/Sell Amount"]),
+					price: parseFloat(data.Price),
+				});
+			})
+			.on("end", () => resolve(results))
+			.on("error", (error) => reject(error));
+	});
+};
+
+module.exports = { parseCSV };
